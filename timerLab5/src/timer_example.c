d81a3cd7265b90de8a16718721ee65c098d73acd
Harsh Shah <shahharsh903@gmail.com>
2021-10-06 14:41:44 -0400

    custom delay function using tim11

--- timerLab5/src/timer_example.c
+++ timerLab5/src/timer_example.c
@@ -1,11 +1,8 @@
 /*******************************************************************************
-  * File Name          : adc_example.c
-  * Description        : Allows user to view graphically the value of adc pins A4, A5.
-                       : This src code follows the template provided in the stm32montor.
+  * File Name          : timer_example.c
+  * Description        : Allows user to control TIM11 with user defined precice custom delay.                       
                        : It also takes the ref from the following websites. 
-                       : https://stackoverflow.com/questions/14539867/how-to-display-a-progress-indicator-in-pure-c-c-cout-printf
-                       : https://www.mccdaq.com/PDFs/specs/Analog-to-Digital.pdf
-                       : https://learn.sparkfun.com/tutorials/analog-to-digital-conversion/relating-adc-value-to-voltage 
+                       : https://deepbluembedded.com/stm32-timer-interrupt-hal-example-timer-mode-lab/
   * Author             : Harsh Hirenbhai Shah
                        : Arsh Sandhu
   * Date               : Oct-1-2021
@@ -19,44 +16,113 @@
 #include "main.h"
 
 
+
+//tim11 handel
+TIM_HandleTypeDef htim11;
+
 /**
-  * @brief initialize the timer pins.
-  * @param int channel
-  * @retval void 
+  * @brief Nano second delay function.
+  * @param uint16_t time
+  * @retval None
   */
-void timerInit(){
-  
+void DelayNS (uint16_t us)
+{
+	TIM11->CNT = 0;
+	while (TIM11->CNT < us);
 }
 /**
-  * @brief reads any of the two channel ADC.
-  * @param int channel
-  * @retval reading of the selected adc channel, returns -1 if selected channel other then 10 or 11 are selected 
+  * @brief Microseconds second delay function.
+  * @param uint16_t time
+  * @retval None
   */
+void DelayMS (uint16_t ms)
+{
+	for (uint16_t i=0; i<ms; i++)
+	{
+		DelayNS (1000); // 1 ms = 1000 ns.
+	}
+}
 
- int getReadingFromAdc(int channel)
- {
-  
-  uint32_t val;
-
-  return val;
- }
-
-
-  
 
 /**
-  * @brief GPIO Event checker, This function allows user to take turn on or off action on various GPIOs.
+  * @brief TIM11 Initialization Function
+  * @param None
+  * @retval None
+  */
+void timerInit() {
+  htim11.Instance = TIM11;
+  htim11.Init.Prescaler = 90-1;
+  htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim11.Init.Period = 65535;
+  htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
+  {
+    printf("could not initialize the timer.\n");
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+void gpioInit() {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+} 
+
+/**
+  * @brief Timer event function
   * @param int mode of the event, does it require interaction?
   * @retval ParserReturnVal_t enum. 
   */
 
 ParserReturnVal_t timerEvent(int mode)
 {
-  
+  gpioInit();
+  timerInit();
+  HAL_TIM_Base_Start(&htim11);
+
   if(mode != CMD_INTERACTIVE) return CmdReturnOk;
 
+   uint32_t delay, rc;
+   
+  rc=fetch_uint32_arg(&delay);
+  if(rc)
+  {
+    printf("Delay must be provided.\n");
+    return CmdReturnBadParameter1;
+  }
 
+  printf("Built LED will be turnded ON and OFF for %ld microseconds \n", delay);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	DelayMS(delay);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	DelayMS(delay);
+  WDTFeed();
   return CmdReturnOk;
 }
 
-ADD_CMD("tdelay",timerEvent,"                timerdelay <delay>")
+ADD_CMD("tdelay",timerEvent,"                timerdelay <microseconds delay>")

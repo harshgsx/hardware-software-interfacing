34792de485386fe1b24117f4b7694840223b1230
Harsh Shah <shahharsh903@gmail.com>
2021-11-10 13:53:18 -0500

    enhancenment to lab7, 8

--- pwmLab8/src/pwm_example.c
+++ pwmLab8/src/pwm_example.c
@@ -1,14 +1,3 @@
-/*******************************************************************************
-  * File Name          : timer_example.c
-  * Description        : Allows user to control TIM11 with user defined precice custom delay.                       
-                       : It also takes the ref from the following websites. 
-                       : https://deepbluembedded.com/stm32-timer-interrupt-hal-example-timer-mode-lab/
-  * Author             : Harsh Hirenbhai Shah
-                       : Arsh Sandhu
-  * Date               : Oct-6-2021
-  ******************************************************************************
-  */
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -17,149 +6,207 @@
 #include "common.h"
 #include "main.h"
 
+TIM_HandleTypeDef tim1;			// handle for timer1 used
 
+int32_t steps;					//declared as global vaiable as used in multiple
+uint32_t delayVal; 
 
-//tim11 handel
-TIM_HandleTypeDef htim1;
+void setRGB (uint8_t red, uint8_t green, uint8_t blue);
 
+ParserReturnVal_t CmdPWMInit(int action)
+{
 
+    if (action == CMD_SHORT_HELP)
+        return CmdReturnOk;
+    if (action == CMD_LONG_HELP)
+    {
+        printf("timinit\n\n"
+               "This command initialize timer 1 PWM channels and its respective gpio pins\n");
 
-/**
- * @brief  Transform the input RGB value to PWM scale
- * @param  Value for mapping 0-255
- * @param  Input starting range e.g. 0
- * @param  Input ending range  e.g 255
- * @param  Output starting range e.g 0
- * @param  Output ending range   e.g 9090
- * @retval mapped PWM value within output specified range.
- */
-int32_t mapRGBToPWM(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max){
-	return (x) * ((out_max - out_min) / (in_max - in_min));
-}
-
-/**
- * @brief  glow RGB LED by specifying Red, Green and Blue channel colors.
- * @param  Red 		from 0 to 255
- * @param  Green 	from 0 to 255
- * @param  Blue 	from 0 to 255
- * @retval None
- */
-void glowRGB(uint8_t red, uint8_t blue, uint8_t green){
-
-	//STOP all three O/P channel
-	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_3);
-
-	//Get RGB values to PWM values.
-	__HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, mapRGBToPWM(green, 0, 255, 0, 9090));
-	__HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, mapRGBToPWM(blue, 0, 255, 0, 9090));
-	__HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_3, mapRGBToPWM(red, 0, 255, 0, 9090));
-
-	//Apply the PWM signals as updated state.
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
-
-}
-/**
-  * @brief TIM11 Initialization Function
-  * @param None
-  * @retval None
-  */
-void timer1Init() {
-
+        return CmdReturnOk;
+    }
+    
+    /* Initialize PWM */
 __HAL_RCC_TIM1_CLK_ENABLE();
-htim1.Instance = TIM1;
-htim1.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;
-htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-htim1.Init.Period = 1000;
-htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-htim1.Init.RepetitionCounter = 0;
-HAL_TIM_Base_Init(&htim1);
+tim1.Instance = TIM1;
+tim1.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;
+tim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+tim1.Init.Period = 1000;
+tim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+tim1.Init.RepetitionCounter = 0;
+HAL_TIM_Base_Init(&tim1);
 
 TIM_OC_InitTypeDef sConfig;
 sConfig.OCMode = TIM_OCMODE_PWM1;
 sConfig.Pulse = 500;
 sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
 sConfig.OCNPolarity = TIM_OCNPOLARITY_LOW;
-sConfig.OCFastMode= TIM_OCFAST_DISABLE;
+sConfig.OCFastMode = TIM_OCFAST_DISABLE;
 sConfig.OCIdleState = TIM_OCIDLESTATE_RESET;
-sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-HAL_TIM_PWM_ConfigChannel(&htim1,&sConfig, TIM_CHANNEL_1);
+sConfig.OCNIdleState =TIM_OCNIDLESTATE_RESET;
 
-}
+HAL_TIM_PWM_ConfigChannel(&tim1,&sConfig,TIM_CHANNEL_1);
+HAL_TIM_PWM_ConfigChannel(&tim1,&sConfig,TIM_CHANNEL_2);
+HAL_TIM_PWM_ConfigChannel(&tim1,&sConfig,TIM_CHANNEL_3);
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-void gpioInit() {
-
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+GPIO_InitTypeDef GPIO_InitStruct = {0};
 __HAL_RCC_GPIOA_CLK_ENABLE();
 /* Configure the PWM output pins */
-GPIO_InitStruct.Pin = (GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8);
+GPIO_InitStruct.Pin = (GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10);
 GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
 GPIO_InitStruct.Pull = GPIO_NOPULL;
 GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 GPIO_InitStruct.Alternate = 1;
 HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-} 
-
-/**
-  * @brief Timer event function
-  * @param int mode of the event, does it require interaction?
-  * @retval ParserReturnVal_t enum. 
-  */
-ParserReturnVal_t pwmInit(int mode){
-
-  if(mode != CMD_INTERACTIVE) return CmdReturnOk;
-
-    gpioInit();
-    timer1Init();
-
-  return CmdReturnOk;
+    return CmdReturnOk;
 }
-ADD_CMD("pwminit",pwmInit,"              Initialize PWM pins") 
+
+ADD_CMD("pwmi", CmdPWMInit, "pwm init")
 
 
-ParserReturnVal_t timerEvent(int mode)
+ParserReturnVal_t CmdPWM(int mode)
 {
-  if(mode != CMD_INTERACTIVE) return CmdReturnOk;
 
- for(int x = 0; x < 50; x++) {
-   
-        float pwm_val = 255.0*sqrt(1.0 -  pow(abs((2.0*(x/500))-1.0),2.0));
-        int pwmToInt = (int) pwm_val;
-        printf("%lf, %d\n",pwm_val, pwmToInt);
-        //glowRGB(100,120, 150);
+  uint32_t waveType, rcWaveType;
+  uint32_t brightness, rcBrightness;
+  uint32_t repeateCount, rcRepeateCount;
 
-        TIM1->CCR1 = pwmToInt;
-        HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);        
-        HAL_Delay(500);
-        HAL_TIM_PWM_Stop(&htim1,TIM_CHANNEL_1);
-        WDTFeed();
+  if((mode != CMD_INTERACTIVE)) return CmdReturnOk;
 
-      }
+  rcWaveType = fetch_uint32_arg(&waveType);
+  if(rcWaveType) {
+    printf("Please provide wave type either 0 or 1. Default is circular wave : %ld\n",waveType);
+    waveType = 0;
+  }
+
+  rcBrightness = fetch_uint32_arg(&brightness);
+  if(rcBrightness) {
+    printf("Please provide brighness between 1 & 500. Default is 500: %ld\n", brightness);
+    brightness = 500;
+
+  }
+
+  rcRepeateCount = fetch_uint32_arg(&repeateCount);
+  if(rcRepeateCount) {
+    printf("Please repeate count should greater than 0. Default is 25: %ld\n",repeateCount);   
+    repeateCount = 25;
+  }
+
+
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_1);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_2);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_3);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_3);
+
+  tim1.Instance->CCR1 = 0;
+  tim1.Instance->CCR2 = 0;
+  tim1.Instance->CCR3 = 0;
+
+
+volatile float pwm_value = 0.0f;
+volatile float brightnessInFloat = brightness * 1.0f;
+
+for(int j = 0; j < repeateCount; j++)
+{
+  //printf("%d\n",j);
+  for(int ii = 0; ii < brightness; ii++)
+  {
+    if(waveType == 1)
+    {
+       pwm_value = 255.0 * (1.0 - abs((int)(2.0*(ii/brightnessInFloat) - 1.0)));
+    } else if(waveType == 0)
+    {
+      pwm_value = 255.0 * sqrt(1.0 - pow(abs((int)(2.0*(ii/brightnessInFloat)) - 1.0),2.0));
+    }
+
+    tim1.Instance->CCR1 = (int)pwm_value;
+    tim1.Instance->CCR2 = (int)pwm_value;
+    tim1.Instance->CCR3 = (int)pwm_value;
+    //printf("%d\n",ii);
+    HAL_Delay(1);
+    WDTFeed();
+  }
+  HAL_Delay(10);
+}
+  
+	HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_1);
+  HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_2);
+  HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_3);
+  
+  return CmdReturnOk;
+}
+ADD_CMD("pwm",CmdPWM,"pwm <wave-type> <brightness> <repeat> 0|1 0 = tringular wave, 1 = circular wave")
+
+
+ParserReturnVal_t CmdRGB(int mode)
+{
+  uint32_t red_channel, green_channel, blue_channel, rc_red, rc_green, rc_blue;
+  
+ 
+
+  rc_red = fetch_uint32_arg(&red_channel);
+  if(rc_red) {
+    printf("Please specify red cahnnel value\n");
+    return CmdReturnBadParameter1;
+  }
+
+  rc_green = fetch_uint32_arg(&green_channel);
+  if(rc_green) {
+    printf("Please specify green cahnnel value\n");
+    return CmdReturnBadParameter1;
+  }
+
+  rc_blue = fetch_uint32_arg(&blue_channel);  
+  if(rc_blue) {
+    printf("Please specify blue cahnnel value\n");
+    return CmdReturnBadParameter1;
+  }
+
+
+
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_1);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_2);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&tim1,TIM_CHANNEL_3);
+  HAL_TIMEx_PWMN_Start(&tim1, TIM_CHANNEL_3);
+
+
+  setRGB (red_channel,green_channel,blue_channel);  
+
 
   return CmdReturnOk;
 }
+ADD_CMD("rgb",CmdRGB,"rgb R|G|B")
 
-// void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-// {
-//   // Check which version of the timer triggered this callback and toggle LED
-//   if (htim == &htim11 )
-//   { 
-//     //uint32_t timer_val; 
-//     __HAL_TIM_SET_COUNTER(&htim11, 0);
-//     //timer_val = __HAL_TIM_GET_COUNTER(&htim11);
-//     //printf("Ticks - In callback: %ld\n", timer_val);
-//     WDTFeed();
-//     //TIM11->CNT = 0;
-//   }
-// }
 
-ADD_CMD("pwm",timerEvent,"<channel>  <value>")
+ParserReturnVal_t CmdStopPWM(int mode)
+{
+	uint32_t rc, val;
+	
+	if (mode != CMD_INTERACTIVE) return CmdReturnOk;
+
+	rc = fetch_uint32_arg(&val); 
+	if (rc) 
+	HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_1);
+  HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_2);
+  HAL_TIM_PWM_Stop(&tim1,TIM_CHANNEL_3);
+	
+	return CmdReturnOk;
+}
+
+ADD_CMD("pwmstop", CmdStopPWM,"                         To Stop timer")
+
+
+void setRGB (uint8_t red, uint8_t green, uint8_t blue)
+{
+  tim1.Instance->CCR1 = red;
+  tim1.Instance->CCR2 = green;
+  tim1.Instance->CCR3 = blue;
+
+  HAL_Delay(10);
+}
+
--- pwmLab8/src/stepper_example.c
+++ pwmLab8/src/stepper_example.c
@@ -1,260 +1,256 @@
-// /*******************************************************************************
-//   * File Name          : stepper_example.c
-//   * Description        : This program allows user to controll the Speed, Steps and Rotation degree of the stepper motor. 
-//                        : It utilizes the NS and MS timer function from the timer lab that uses TIM11 without interrupts.
-//                        : D6 -> STEP, D5 -> FR and A2 -> VREF  
-//   * Author             : Harsh Hirenbhai Shah
-//                        : Arsh Sandhu
-//   * Date               : Oct-13-2021
-//   ******************************************************************************
-//   */
+// CNTR8005 example of using a timer to create an interrupt
+// Allan Smith
+// Oct 12, 2021
+#include <stdio.h>
+#include <stdint.h>
 
-// #include <stdio.h>
-// #include <stdint.h>
+#include "common.h"
+#include "main.h"
 
-// #include "common.h"
-// #include "main.h"
+TIM_HandleTypeDef tim11;												
 
 
+int gDirection = 0;
+int gIsStepperEnabled = 0;
 
-// //tim11 handel
-// TIM_HandleTypeDef htim11;
-// int isStepperEnabled = -1;
+void Timer11_Init(int pulseDuration){
 
-// /**
-//   * @brief Nano second delay function.
-//   * @param uint16_t time
-//   * @retval None
-//   */
-// void DelayNS (uint16_t us)
-// {
-// 	TIM11->CNT = 0;
-// 	while (TIM11->CNT < us);
-// }
-// /**
-//   * @brief Microseconds second delay function.
-//   * @param uint16_t time
-//   * @retval None
-//   */
-// void DelayMS (uint16_t ms)
-// {
-// 	for (uint16_t i=0; i<ms; i++)
-// 	{
-// 		DelayNS (1000); // 1 ms = 1000 ns.
-// 	}
-// }
+    __HAL_RCC_TIM11_CLK_ENABLE();
+	tim11.Instance = TIM11;
+	tim11.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000000 - 1;			// this will cause the timer to create ms (set to 1000000 - 1 for us)
+	tim11.Init.CounterMode = TIM_COUNTERMODE_UP;
+	tim11.Init.Period = pulseDuration/2;											// in this example the number here will be the number of ms for on/off
+	tim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	tim11.Init.RepetitionCounter = 0;
+	tim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;		// this parameter will cause the timer to reset when timed out
+	HAL_TIM_Base_Init(&tim11);
+
+	HAL_NVIC_SetPriority(TIM1_TRG_COM_TIM11_IRQn, 10, 0U);				// note the timer 11 IRQ on this board is shared with Timer 1
+	HAL_NVIC_EnableIRQ(TIM1_TRG_COM_TIM11_IRQn);
+
+HAL_TIM_Base_Start_IT(&tim11);
+	
+}
 
 
-// /**
-//   * @brief TIM11 Initialization Function
-//   * @param None
-//   * @retval None
-//   */
-// void timerInit() {
+void gpioInit(){
+   GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA4 LD2_Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB10 PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+//Telling motor to rotate CW or CCW
+HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+
+//Checking if we need to switch on the Motor.
+HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);  
+}
+
+ParserReturnVal_t  enableDisableStepper(int mode)
+{
+  if(mode != CMD_INTERACTIVE) return CmdReturnOk;
   
-//   __HAL_RCC_TIM11_CLK_ENABLE();
-//   htim11.Instance = TIM11;
-// 	htim11.Init.Prescaler = HAL_RCC_GetPCLK2Freq() / 1000 - 1;			// this will cause the timer to create ms (set to 1000000 - 1 for us)
-// 	htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
-// 	htim11.Init.Period = 60000000/4096/2;											// in this example the number here will be the number of ms for on/off
-// 	htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-// 	htim11.Init.RepetitionCounter = 0;
-// 	htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;		// this parameter will cause the timer to reset when timed out
-// 	HAL_TIM_Base_Init(&htim11);
+  uint32_t stepperStatus, rc;
 
-// 	HAL_NVIC_SetPriority(TIM1_TRG_COM_TIM11_IRQn, 10, 0U);				// note the timer 11 IRQ on this board is shared with Timer 1
-// 	HAL_NVIC_EnableIRQ(TIM1_TRG_COM_TIM11_IRQn);
+  //taking the input from user for enabling and disabling the stepper motor. 
+  rc=fetch_uint32_arg(&stepperStatus);
+  if(rc)
+  {
+    printf("Please enable or disable stepper controller.\n");
+    return CmdReturnBadParameter1;
+  }
 
-// 	HAL_TIM_Base_Start_IT(&htim11);
-//   // htim11.Instance = TIM11;
-//   // htim11.Init.Prescaler = 80;
-//   // htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
-//   // htim11.Init.Period = 65535;
-//   // htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-//   // htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-//   // if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
-//   // {
-//   //   printf("could not initialize the timer.\n");
-//   // }
+  //validating against the user input and reflecting it to the global variable.
+  if(stepperStatus == 1)
+  {   
+    gIsStepperEnabled = 1; 
+    printf("stepper motor enabled.\n");
+  } else if (stepperStatus == 0)
+  {
+    gIsStepperEnabled = 0; 
+    printf("stepper motor disabled.\n");
+  }
 
-//   // HAL_TIM_Base_Start(&htim11);
-// }
+  return CmdReturnOk;
+}
+ADD_CMD("se",enableDisableStepper,"         enable or disable stepper.\n");
 
-// /**
-//   * @brief GPIO Initialization Function for stepper motors
-//   * @param None
-//   * @retval None
-//   */
-// void stepperInit() {
-//    GPIO_InitTypeDef GPIO_InitStruct = {0};
+ParserReturnVal_t StepSpeedAction(int mode)
+{
+	uint32_t dir, rcDir, delay,rcDelay;
+	  
+	if(mode != CMD_INTERACTIVE) return CmdReturnOk;
 
-//   /* GPIO Ports Clock Enable */
-//   __HAL_RCC_GPIOC_CLK_ENABLE();
-//   __HAL_RCC_GPIOH_CLK_ENABLE();
-//   __HAL_RCC_GPIOA_CLK_ENABLE();
-//   __HAL_RCC_GPIOB_CLK_ENABLE();
+    rcDir = fetch_uint32_arg(&dir);
 
-//   /*Configure GPIO pin Output Level */
-//   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|LD2_Pin, GPIO_PIN_RESET);
+    rcDelay = fetch_uint32_arg(&delay);
 
-//   /*Configure GPIO pin Output Level */
-//   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_4, GPIO_PIN_RESET);
+    if(rcDelay || rcDir) {
+    printf("Please specify the direction and delay.\n");
+     return CmdReturnBadParameter1;
+    }
 
-//   /*Configure GPIO pin : B1_Pin */
-//   GPIO_InitStruct.Pin = B1_Pin;
-//   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-//   GPIO_InitStruct.Pull = GPIO_NOPULL;
-//   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+    gDirection = dir;
 
-//   /*Configure GPIO pins : PA4 LD2_Pin */
-//   GPIO_InitStruct.Pin = GPIO_PIN_4|LD2_Pin;
-//   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-//   GPIO_InitStruct.Pull = GPIO_NOPULL;
-//   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-//   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    if(gIsStepperEnabled != 1)
+    {
+        printf("Please enable stepper motor.");
+        return CmdReturnOk;
+    }
+    else 
+    {
+    
+    printf("Inside motor init\n");
+    gpioInit();
+    Timer11_Init(delay);
+    HAL_TIM_Base_Start_IT(&tim11);
 
-//   /*Configure GPIO pins : PB10 PB4 */
-//   GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_4;
-//   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-//   GPIO_InitStruct.Pull = GPIO_NOPULL;
-//   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-//   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-// } 
+    }
 
-// /**
-//   * @brief Moter event function
-//   * @param int mode of the event, does it require interaction?
-//   * @retval ParserReturnVal_t enum. 
-//   */
-// ParserReturnVal_t motorEvent(int mode)
-// {
-//   stepperInit();
-//   timerInit();
-  
+	return CmdReturnOk;
+}
 
-//   if(mode != CMD_INTERACTIVE) return CmdReturnOk;
-
-// if(isStepperEnabled == 0)
-// {
-//   printf("Please enable the stepper drive first. See help for more details.\n");
-//   return CmdReturnOk;
-// } 
-
- 
-// uint32_t delay, rc, step = 0,rcTwo, isCW, rcisCW;
-  
-//   // Delay Input    
-//   rc=fetch_uint32_arg(&delay);
-//   if(rc)
-//   {
-//     printf("Delay must be provided.\n");
-//     return CmdReturnBadParameter1;
-//   }
-
-//   //Step Input
-//   rcTwo = fetch_uint32_arg(&step);
-//   if(rcTwo)
-//   {
-//     step = 400;
-//     printf("User did not provided steps, default steps are 400.\n");
-//   }
-
-//   //Rotation Input
-//   rcisCW = fetch_uint32_arg(&isCW);
-//   if(rcisCW)
-//   {
-//     isCW = 1;
-//     printf("User did not provide the direction, so running in CW.\n");
-//   }  
-
-//   //Telling motor to rotate CW or CCW
-//   if(isCW == 1)
-//   {
-//     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
-//   } 
-//   else if(isCW == 0)
-//   {
-//     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
-//   }
-
-//   //Checking if we need to switch on the Motor.
-//   if(isStepperEnabled == 1)
-//   {
-//     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);  
-//   } else {
-//     printf("Added another check if we need to turn on the moter. \n");
-//     return CmdReturnOk;
-//   }
-
-//   //Rotating the motor
-// 	  // for(int x = 0; x<step; x++)
-// 	  // {
-//     //   //printf("inside for loop %d\n", x);
-// 		//   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
-// 		//   DelayNS(delay);
-// 		//   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
-// 		//   DelayNS(delay);
-//     //   WDTFeed();
-// 	  // }
-
-//   //WDTFeed();
-//   return CmdReturnOk;
-// }
+ADD_CMD("ss",StepSpeedAction,"<dir> <delay>    Make stepper step at a speed set by the delay value.\n");
 
 
-// ADD_CMD("step",motorEvent,"                step <delay> <step> <direction> 0=CW, 1=CCW");
+ParserReturnVal_t StepSpeedQAction(int mode)
+{
+	uint32_t dir;
+    uint32_t rcDir;
+    uint32_t delay1;
+    uint32_t delay2;
+    uint32_t delay3;
+    uint32_t rcDelay1;
+    uint32_t rcDelay2;
+    uint32_t rcDelay3;
+    uint32_t stepSeq = 0;
+	  
+	if(mode != CMD_INTERACTIVE) return CmdReturnOk;
 
-// /**
-//   * @brief enable Disable stepper motor event.
-//   * @param int mode, do we need to run this function interactively?
-//   * @retval ParserReturnVal_t return code. 
-//   */
-// ParserReturnVal_t  enableDisableStepper(int mode)
-// {
-//   if(mode != CMD_INTERACTIVE) return CmdReturnOk;
-  
-//   //stepper and timer init
-//   stepperInit();
-//   timerInit();
+    rcDir = fetch_uint32_arg(&dir);
+    rcDelay1 = fetch_uint32_arg(&delay1);
+    rcDelay2 = fetch_uint32_arg(&delay2);
+    rcDelay3 = fetch_uint32_arg(&delay3);
 
-//   uint32_t stepperStatus, rc;
+    if(rcDelay1 || rcDelay2 || rcDelay3 || rcDir ) {
+    printf("Please specify the direction and delay.\n");
+     return CmdReturnBadParameter1;
+    }
 
-//   //taking the input from user for enabling and disabling the stepper motor. 
-//   rc=fetch_uint32_arg(&stepperStatus);
-//   if(rc)
-//   {
-//     printf("Please enable or disable stepper controller.\n");
-//     return CmdReturnBadParameter1;
-//   }
+    // if(delay1 > delay2){
+    // printf("Motor should start with a slower speed.\n");
+    //  return CmdReturnBadParameter1;
+    // }
 
-//   //validating against the user input and reflecting it to the global variable.
-//   if(stepperStatus == 1)
-//   {   
-//     isStepperEnabled = 1; 
-//     printf("stepper motor enabled.\n");
-//   } else if (stepperStatus == 0)
-//   {
-//     isStepperEnabled = 0; 
-//     printf("stepper motor disabled.\n");
-//   }
-  
-  
-// return CmdReturnOk;
-// }
+    gDirection = dir;
 
-// ADD_CMD("se",enableDisableStepper,"                stepperenable <0|1>");
+    if(gIsStepperEnabled != 1)
+    {
+        printf("Please enable stepper motor.");
+        return CmdReturnOk;
+    }
+    else 
+    {
+    for(int i = 0; i < 2048; i++)
+    {
+        if(stepSeq == 0 && i == 0) 
+        {
+            printf("Starting with delay 1 %ld..\n", delay1);
+            gpioInit();
+            Timer11_Init(delay1);
+            
+        } else if(stepSeq == 0 && i == 2048 - 1){
+            i = 0;
+            stepSeq = 1;
+        } else if(stepSeq == 1 && i == 0) 
+        {
+            printf("Starting with delay 2 %ld..\n", delay2);
+            gpioInit();
+            Timer11_Init(delay2);
+     
+        } else if(stepSeq == 1 && i == 2048 - 1){
+            i = 0;
+            stepSeq = 2;
+        } else if(stepSeq == 2 && i == 0) 
+        {
+            printf("Starting with delay 2 %ld..\n", delay2);
+            gpioInit();
+            Timer11_Init(delay2);
+            
+        } else if(stepSeq == 2 && i == 2048 - 1){
+            printf("Stopping.\n");
+           
+        }
+    }
+    }
+
+	return CmdReturnOk;
+}
+
+ADD_CMD("sq",StepSpeedQAction,"<dir> <delay1> <delay2> <delay3>           Make stepper step at a speed set by the delay value.\n");
+
+void TIM1_TRG_COM_TIM11_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&tim11);
+}
 
 
-// /**/
-// void TIM1_TRG_COM_TIM11_IRQHandler(void)
-// {
-// 	HAL_TIM_IRQHandler(&htim11);
-// }
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim )
+{
+	if( htim == &tim11 ){								// check if this was meant for us
+		HAL_GPIO_TogglePin( LD2_GPIO_Port, LD2_Pin );
+        
+        //Telling motor to rotate CW or CCW
+        if(gDirection < 0)
+        {
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+        } 
+        else if(gDirection == 0)
+        {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+        }
+        else if(gDirection > 0)
+        {
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+        }
+        
+        if(gIsStepperEnabled == 1)
+        {
+            //Checking if we need to switch on the Motor.
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);              
+            HAL_GPIO_TogglePin( GPIOB, GPIO_PIN_10 );
+        }
 
-// void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim )
-// {
-// 	if( htim == &htim11 ){		
-//      //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);						// check if this was meant for us
-// 		HAL_GPIO_TogglePin( GPIOB, GPIO_PIN_10 );
-// 	}
-// }
+
+        
+	}
+}
